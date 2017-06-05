6d0fe931da63052fb68b1b505cf46707d3ccdc67
Éverton Inocêncio <hewerthomn@gmail.com>
2017-06-04 21:05:50 -0400

    adicionado leds individuais para cada um

--- led_clock_v1.ino
+++ led_clock_v1.ino
@@ -5,20 +5,24 @@
 #define PIN_RTC_DATA  7
 #define PIN_RTC_RST   8
 
-#define PIN_LED_DATA 3
-#define NUM_LEDS     24
-#define BRIGHTNESS   50
+#define PIN_LED_HOURS   3
+#define PIN_LED_MINUTES 4
+#define PIN_LED_SECONDS 5
+#define BRIGHTNESS_MIN  50
+#define BRIGHTNESS_MAX  255
 #define LED_FPS      1
 
-CRGB leds[NUM_LEDS];
+CRGB ledsHours[12];
+CRGB ledsMinutes[12];
+CRGB ledsSeconds[24];
 
 int _day        = 3,
     _month      = 6,
     _year       = 2017,
     _dayofweek  = 7,
-    _hours      = 7,
-    _minutes    = 51,
-    _seconds    = 14,
+    _hours      = 11,
+    _minutes    = 25,
+    _seconds    = 00,
     actualHours,
     actualMinutes,
     actualSeconds
@@ -35,77 +39,104 @@ void setup() {
   //    setDS1302Time(seconds, minutes, hours, dayofweek, day, month, year)
 //  myRTC.setDS1302Time(_seconds, _minutes, _hours, _dayofweek, _day, _month, _year);
 
-  FastLED.addLeds<WS2811, PIN_LED_DATA, RGB>(leds, NUM_LEDS);
-  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.addLeds<WS2811, PIN_LED_HOURS, RGB>(ledsHours, 12);
+  FastLED.setBrightness(BRIGHTNESS_MAX);
+  
+  FastLED.addLeds<WS2811, PIN_LED_MINUTES, RGB>(ledsMinutes, 12);
+  FastLED.setBrightness(BRIGHTNESS_MIN);
+
+  FastLED.addLeds<WS2811, PIN_LED_SECONDS, RGB>(ledsSeconds, 24);
+  FastLED.setBrightness(BRIGHTNESS_MAX);
 }
 
 void loop() {
   printClock();  
   showLedClock();
-  
-  delay(1000);
-//  turnOffLeds();
 }
 
 void showLedClock()
 {
   int ledHours = 0;
   int ledMinutes = 0;
+  int ledSeconds = 0;
 
   myRTC.updateTime();
-  
-  actualSeconds = myRTC.seconds;  
 
   if (actualHours != myRTC.hours) {
     actualHours = myRTC.hours;
     
     ledHours = (actualHours > 12) ? (actualHours - 12) : actualHours;  
     ledHours = ledHours - 1;
-    leds[ledHours] = CRGB::Blue;
-    changeTime(true);
-    leds[ledHours] = CRGB::Blue;
+    ledsHours[ledHours] = CRGB::Red;
+    changeTime(1);
+    ledsHours[ledHours] = CRGB::Red;
 
     Serial.print("ledHours: ");Serial.println(ledHours);    
   } 
 
   if (actualMinutes != myRTC.minutes) {
     actualMinutes = myRTC.minutes;    
-    ledMinutes = map(actualMinutes, 0, 55, 12, 23);
-    leds[ledMinutes] = CRGB::Red;
-    changeTime(false);
-    leds[ledMinutes] = CRGB::Red;
-    Serial.print("ledMinutes: ");Serial.println(ledMinutes);    
+    ledMinutes = map(actualMinutes, 0, 55, 0, 11);
+    ledsMinutes[ledMinutes] = CRGB::Blue;
+    changeTime(2);
+    ledsMinutes[ledMinutes] = CRGB::Blue;
+    Serial.print("ledMinutes: ");Serial.println(ledMinutes);
+  }
+
+  if (actualSeconds != myRTC.seconds) {
+    actualSeconds = myRTC.seconds;
+
+    ledSeconds = map(actualSeconds, 0, 59, 0, 23);
+    ledsSeconds[ledSeconds] = CRGB::White;
+    changeTime(3);
+    ledsSeconds[ledSeconds] = CRGB::White;
+    Serial.print("ledSeconds: ");Serial.println(ledSeconds);
   }
 
   FastLED.show();
 //  FastLED.delay(1000 / LED_FPS);
 }
 
-void changeTime(bool isHour)
+void changeTime(int t)
 {
-  FastLED.show();
-  delay(1000);  
-  if (isHour) {
-    turnOffLedsHour();
-  } else {
-    turnOffLedsMinutes();
-  }
+  FastLED.show();  
+  delay(500);
+  switch (t)
+  {
+    case 1:
+      turnOffLedsHour();      
+      break;
+    case 2:
+      turnOffLedsMinutes();
+      break;
+    case 3:
+      turnOffLedsSeconds();
+      break;
+  }  
 }
 
 void turnOffLedsHour()
 {
   for (int i = 0; i <= 11; i++) {
-    leds[i] = CRGB::Black;
+    ledsHours[i] = CRGB::Black;
   }  
   FastLED.show();
 }
 
 void turnOffLedsMinutes()
 {
-  for (int i = 12; i <= 23; i++) {
-    leds[i] = CRGB::Black;
+  for (int i = 0; i <= 11; i++) {
+    ledsMinutes[i] = CRGB::Black;
   }  
   FastLED.show();  
+}
+
+void turnOffLedsSeconds()
+{
+  for (int i = 0; i <= 23; i++) {
+    ledsSeconds[i] = CRGB::Black;
+  }  
+  FastLED.show();
 }
 
 void printClock()
